532e7caab1311495703bde6a8810d5489ecf175e
mariasodiaz <mariasol.diaz@mercadolibre.com>
2022-10-24 15:34:25 -0300

    refactor

--- internal/products/service_test.go
+++ internal/products/service_test.go
@@ -33,7 +33,23 @@ func TestGetAll(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, expected, result)
 
-	result, err = service.GetAllBySeller("1")
+}
+
+func TestIntegration(t *testing.T) {
+	repo := NewRepository()
+	service := NewService(repo)
+	expected := []Product{{ID: "mock", SellerID: "FEX112AC", Description: "generic product", Price: 123.55}}
+	result, err := service.GetAllBySeller("FEX112AC")
+	assert.Nil(t, err)
+	assert.Equal(t, expected, result)
+}
+
+func TestGetAllBad(t *testing.T) {
+	products := []Product{{ID: "1", SellerID: "123", Description: "una descripcion", Price: 1000.5}, {ID: "2", SellerID: "AC234", Description: "otra descripcion", Price: 100.12}}
+	mock := MockRepository{Products: products}
+	service := NewService(mock)
+
+	result, err := service.GetAllBySeller("1")
 	assert.Nil(t, result)
 	assert.NotNil(t, err)
 }
